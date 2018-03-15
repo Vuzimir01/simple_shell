66faa675ba10fbbcc1060e245e4ad3d8aad2a2a5
Nick <nickolasteixeira01@gmail.com>
2018-03-15 21:49:29 +0000

    change printf functions to write

--- test_folder/print_all_arguments.c
+++ test_folder/print_all_arguments.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
+#include <unistd.h>
+unsigned int _strlen(char *s)
+{
+	unsigned int counter = 0;
+	
+	while (s[counter] != '\0')
+		++counter;
+
+	return (counter);
+}
 
 int main(int argc, char **argv)
 {
-	unsigned int i;
+	unsigned int i, length;
 	(void)argc;
 
 	/*find all the arguments in the argv pointer */
 	i = 0;
+	length = 0;
 	while (*(argv + i))
 	{
-		printf("%s ", argv[i]);
+		length = _strlen(argv[i]);
+		write(1, argv[i], length);
 		++i;
 	}
-	printf("\n");
 	return (0);
 }
