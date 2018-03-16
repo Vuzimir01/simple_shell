269b907e49c611af4912e8d18ed3015b3dadbd4b
Nick <nickolasteixeira01@gmail.com>
2018-03-16 01:43:19 +0000

    print the child pid number

--- test_folder/pid.c
+++ test_folder/pid.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include <unistd.h>
-
+#include <stdlib.h>
 int main(void)
 {
-	pid_t my_pid, parent_pid;
-	
+	int my_pid;
+	/*	
 	my_pid = getpid();
 	parent_pid = getppid();
 	printf("PID: %u\n", my_pid);
 	printf("PARENT: %u\n", parent_pid);
+	*/
+	my_pid = fork();
+	if (my_pid == 0)
+	{
+		exit(0);
+	}
 
+	printf("%d\n", my_pid);
 	return (0);
 }
