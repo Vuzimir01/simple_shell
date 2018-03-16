8ff1ffb3cf27db32f81ea414104aaa3e44003d61
Nick <nickolasteixeira01@gmail.com>
2018-03-16 21:50:42 +0000

    initialize header file

---
+++ test_folder/holberton.h
@@ -0,0 +1,14 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+char *_strncpy(char *dest, char *src, int n);
+unsigned int _strlen(char *s);
+unsigned int find_command_length(char *s);
+char **array_from_strtok(char *str);
+
+
+#endif /* _HOLBERTON_H_ */
