ba8e0b51c0e56f43c2d1a1c152daa2b2272e5bfb
Nick <nickolasteixeira01@gmail.com>
2018-03-22 19:28:25 +0000

    accout for non-interactive mode

--- test_folder/prompt.c
+++ test_folder/prompt.c
@@ -15,91 +15,94 @@ int main(void)
 	
 	buffer = NULL;
 	length = 0;
+
+	if (isatty(STDIN_FILENO))
+		write(1, dolla_dolla, 2);
 	
-	write(1, dolla_dolla, 2);
-	while ((characters = getline(&buffer, &length, stdin) != -1))
+	while ((characters = getline(&buffer, &length, stdin)) != EOF)
 	{
-			commands = array_from_strtok(buffer);
-			pid = fork();
-			if (pid == -1)
+		commands = array_from_strtok(buffer);
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("Error:");
+			exit(1);
+		}
+		if (pid == 0)
+		{
+			/* check if commands is NULL or all empty spaces */
+			if (commands == NULL)
 			{
-				perror("Error:");
-				exit(1);
+				free(buffer);
+				exit(0);
 			}
-			if (pid == 0)
+			/* check if the command is a EXIT to exit the shell */
+			else if (_strcmp(exit_command, commands[0]))
 			{
-				/* check if commands is NULL or all empty spaces */
-				if (commands == NULL)
-				{
-					free(buffer);
-					exit(0);
-				}
-				/* check if the command is a EXIT to exit the shell */
-				else if (_strcmp(exit_command, commands[0]))
-				{
-					free_all_double_ptr(commands);
-					free(buffer);	
-					exit(0);
-				}
-				/* check if the command is ENV to print out environment variables */
-				else if (_strcmp(env_command, commands[0]))
-				{
-					free(buffer);
-					free_all_double_ptr(commands);
-					print_env();
-					exit(0);
-				}
-				/* check if the command is a $PATH that has an executable */
-				else if (stat(commands[0], &fileStat) == 0)
-					execve(commands[0], commands, NULL);
-				
-				/* check all $PATH VARIABLES for executable commands */
-				else
-				{
-					i = 0;
-					all_directories = store_env_variables(commands[0]);
-					while (all_directories[i])
-					{
-						if (stat(all_directories[i], &fileStat2) == 0)
-							execve(all_directories[i], commands, NULL);
-						++i;
-					}
-				
-					write(1, "No such file or directory\n", 26);	
-				
-					free(buffer);
-					free_all_double_ptr(commands);	
-					free_all_double_ptr(all_directories);
-					exit(0);
-				}
-			}	/* DON'T FORGET TO FREE YOUR MALLOCS FROM THE TOKEN YOU BUILT */
+				free_all_double_ptr(commands);
+				free(buffer);	
+				exit(0);
+			}
+			/* check if the command is ENV to print out environment variables */
+			else if (_strcmp(env_command, commands[0]))
+			{
+				free(buffer);
+				free_all_double_ptr(commands);
+				print_env();
+				exit(0);
+			}
+			/* check if the command is a $PATH that has an executable */
+			else if (stat(commands[0], &fileStat) == 0)
+				execve(commands[0], commands, NULL);
+			
+			/* check all $PATH VARIABLES for executable commands */
 			else
 			{
-				wait(&status);
-				if (commands == NULL)
+				i = 0;
+				all_directories = store_env_variables(commands[0]);
+				while (all_directories[i])
 				{
-					free(buffer);
-					free_all_double_ptr(commands);
+					if (stat(all_directories[i], &fileStat2) == 0)
+						execve(all_directories[i], commands, NULL);
+					++i;
 				}
-				else if (_strcmp(exit_command, commands[0]))
-				{
-					free(buffer);
-					free_all_double_ptr(commands);
-					exit(0);
-				}
-				else if (commands != NULL)
-				{
-					free_all_double_ptr(commands);	
-					free(buffer);	
-				}
-			}
-
-			length = 0;
-			buffer = NULL;
 			
-			write(1, dolla_dolla, 2);
+				write(1, "No such file or directory\n", 26);	
+			
+				free(buffer);
+				free_all_double_ptr(commands);	
+				free_all_double_ptr(all_directories);
+				exit(0);
+			}
+		}	
+		/* DON'T FORGET TO FREE YOUR MALLOCS FROM THE TOKEN YOU BUILT */
+		else
+		{
+			wait(&status);
 		
-	}
+			if (commands == NULL)
+			{
+				free(buffer);
+				free_all_double_ptr(commands);
+			}
+			else if (_strcmp(exit_command, commands[0]))
+			{
+				free(buffer);
+				free_all_double_ptr(commands);
+				exit(0);
+			}
+			else if (commands != NULL)
+			{
+				free_all_double_ptr(commands);	
+				free(buffer);	
+			}
+		}
+		length = 0;
+		buffer = NULL;
+		
+		if (isatty(STDIN_FILENO))
+			write(1, dolla_dolla, 2);
+	}	
 	if (characters == -1)
 		return (EXIT_FAILURE);
 
